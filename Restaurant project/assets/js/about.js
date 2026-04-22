1b42f92d22b09e495599cd411caa893a6b11a86a
MaryRaafat <maryrafat206@gmail.com>
2026-04-23 01:50:16 +0200

    Add files via upload

---
+++ Restaurant project/assets/js/about.js
@@ -0,0 +1,21 @@
+const toggleBtn = document.getElementById("toggleMode");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
---
+++ Restaurant project/assets/js/main.js
@@ -0,0 +1,40 @@
+function Ahmed() {
+    document.getElementById("fotar").innerHTML =
+        "<p style='font-size:20px;'>Name: Ahmed </p>" +
+        "<p style='font-size:20px;'>AGE: 20</p>" +
+        "<p style='font-size:20px;'>Level: 2</p>" +
+        "<p style='font-size:20px;'>University: EELU</p>"+
+        "<p style='font-size:20px;'>ID: 2300715</p>"
+}
+function Reem() {
+    document.getElementById("fotar").innerHTML =
+        "<p style='font-size:20px;'>Name: Reem </p>" +
+        "<p style='font-size:20px;'>AGE: 20</p>" +
+        "<p style='font-size:20px;'>Level: 2</p>" +
+        "<p style='font-size:20px;'>University: EELU</p>"+
+        "<p style='font-size:20px;'>ID: 2303545</p>"
+}
+function Mary() {
+    document.getElementById("fotar").innerHTML =
+        "<p style='font-size:20px;'>Name: Mary </p>" +
+        "<p style='font-size:20px;'>AGE: 20</p>" +
+        "<p style='font-size:20px;'>Level: 2</p>" +
+        "<p style='font-size:20px;'>University: EELU</p>"+
+        "<p style='font-size:20px;'>ID: 2300000</p>"
+}
+function Fatma() {
+    document.getElementById("fotar").innerHTML =
+        "<p style='font-size:20px;'>Name: Fatma </p>" +
+        "<p style='font-size:20px;'>AGE: 20</p>" +
+        "<p style='font-size:20px;'>Level: 2</p>" +
+        "<p style='font-size:20px;'>University: EELU</p>"+
+        "<p style='font-size:20px;'>ID: 2302592</p>"
+}
+function Christina() {
+    document.getElementById("fotar").innerHTML =
+        "<p style='font-size:20px;'>Name: Christina </p>" +
+        "<p style='font-size:20px;'>AGE: 20</p>" +
+        "<p style='font-size:20px;'>Level: 2</p>" +
+        "<p style='font-size:20px;'>University: EELU</p>"+
+        "<p style='font-size:20px;'>ID: 2300000</p>"
+}
